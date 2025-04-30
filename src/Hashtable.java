60005018bf9461dd62f135f21a45b5f98f98417d
Ahmed_Ragab(Alexandria university) <166864012+A7MEDRAGAB82@users.noreply.github.com>
2025-05-01 00:57:52 +0300

    modifying interface

--- src/Hashtable.java
+++ src/Hashtable.java
@@ -1,6 +1,4 @@
 public interface Hashtable {
-    void insert(String word);          // Insert word
-    boolean contains(String word);     // Check if word exists
-    int getCollisions();               // Get collision count
+
     String printTableStructure();      // New: Returns the visual structure
 }
